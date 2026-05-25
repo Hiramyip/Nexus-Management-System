9f26de3734c6ac4df8fb76956463c445f109ab6b
GaelGlzz <L23100676@nlaredo.tecnm.mx>
2026-05-25 11:35:22 -0500

    feat:Configuracion de prisma

---
+++ prisma.config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"; // <- OBLIGATORIO: Fuerza la carga del archivo .env inmediatamente
+import { defineConfig, env } from "@prisma/config";
+
+// Evaluamos el objetivo de la base de datos
+const isMongo = process.env.PRISMA_TARGET_DB === "mongodb";
+
+export default defineConfig({
+  // Selecciona dinámicamente la ruta del esquema
+  schema: isMongo
+    ? "./prisma/mongodb/schema.prisma"
+    : "./prisma/sqlserver/schema.prisma",
+
+  datasource: {
+    // Si isMongo es true, usa MONGODB_URL. Si no, usa SQL_SERVER_URL.
+    // Usamos env() nativo de Prisma y un string vacío de respaldo para evitar que explote si no se detecta la variable
+    url: isMongo
+      ? (env("MONGODB_URL") || process.env.MONGODB_URL || "")
+      : (env("SQL_SERVER_URL") || process.env.SQL_SERVER_URL || "")
+  }
+});
