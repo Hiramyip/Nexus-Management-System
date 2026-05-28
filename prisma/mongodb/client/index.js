184e557d094883d292a11067af8750e219538afe
RDani1001 <reyesparrareynaldodaniel@gmail.com>
2026-05-28 10:38:50 -0500

    Fix: Cambio de cosas

--- prisma/mongodb/client/index.js
+++ prisma/mongodb/client/index.js
@@ -163,7 +163,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\gael1\\OneDrive\\Escritorio\\uni\\ABD\\proyecto\\nexxus\\Nexus-Management-System\\prisma\\mongodb\\client",
+      "value": "/app/prisma/mongodb/client",
       "fromEnvVar": null
     },
     "config": {
@@ -172,17 +172,16 @@ const config = {
     "binaryTargets": [
       {
         "fromEnvVar": null,
-        "value": "windows",
+        "value": "linux-musl-openssl-3.0.x",
         "native": true
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\gael1\\OneDrive\\Escritorio\\uni\\ABD\\proyecto\\nexxus\\Nexus-Management-System\\prisma\\mongodb\\schema.prisma",
+    "sourceFilePath": "/app/prisma/mongodb/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
-    "schemaEnvPath": "../../../.env"
+    "rootEnvPath": null
   },
   "relativePath": "..",
   "clientVersion": "6.19.3",
@@ -240,8 +239,8 @@ exports.PrismaClient = PrismaClient
 Object.assign(exports, Prisma)
 
 // file annotations for bundling tools to include these files
-path.join(__dirname, "query_engine-windows.dll.node");
-path.join(process.cwd(), "prisma/mongodb/client/query_engine-windows.dll.node")
+path.join(__dirname, "libquery_engine-linux-musl-openssl-3.0.x.so.node");
+path.join(process.cwd(), "prisma/mongodb/client/libquery_engine-linux-musl-openssl-3.0.x.so.node")
 // file annotations for bundling tools to include these files
 path.join(__dirname, "schema.prisma");
 path.join(process.cwd(), "prisma/mongodb/client/schema.prisma")
